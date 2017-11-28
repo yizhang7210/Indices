8dc1f0403125aa2ada74585ca6de02eeb0eeba3c
Yi Zhang <yi.zhang7210@gmail.com>
2017-11-28 09:56:49 -0500

    Poker challenge: handle full house and four of a kind

--- PokerChallenge/src/Hands.hs
+++ PokerChallenge/src/Hands.hs
@@ -1,5 +1,8 @@
 module Hands where
 
+import Data.Map (fromListWith, toList)
+import Data.List (sortBy)
+
 -- Type definitions
 data Suit = C | D | H | S deriving (Show, Eq, Read);
 data Rank = Two | Three | Four | Five | Six | Seven | Eight | Nine |
@@ -20,6 +23,8 @@ type Hand = [Card]
 getHandScore :: Hand -> Int
 getHandScore h
     | isStraightFlush h = 1
+    | isFourOfAKind h = 2
+    | isFullHouse h = 3
     | isFlush h = 4
     | isStraight h = 5
     | otherwise = 9
@@ -32,6 +37,18 @@ isStraight h = maximum ranks == iterate succ (minimum ranks) !! 4 where
     ranks = map rank h
 
 isFlush :: Hand -> Bool
-isFlush h  = and $ map equalFirst suits where
+isFlush h = and $ map equalFirst suits where
     equalFirst = (suit (head h) ==)
     suits = map suit h
+
+getRankCountDesc :: Hand -> [(Rank, Int)]
+getRankCountDesc h = sortBy rankDesc . toList $ fromListWith (+) [(x, 1) | x <- map rank h] where
+    rankDesc = \x y -> (snd y) `compare` (snd x)
+
+isFourOfAKind :: Hand -> Bool
+isFourOfAKind h = snd (getRankCountDesc h !! 0) == 4
+
+isFullHouse :: Hand -> Bool
+isFullHouse h = snd (getRankCountDesc h !! 0) == 3 && snd (getRankCountDesc h !! 1) == 2
+
+
--- PokerChallenge/test/test.hs
+++ PokerChallenge/test/test.hs
@@ -4,15 +4,23 @@ import Hands
 
 main = defaultMain tests
 
+-- Example hands
 straightFlush :: Hand
 straightFlush = [Card Seven S, Card Eight S, Card Nine S, Card Ten S, Card Jack S]
 
-straight :: Hand
-straight = [Card Seven S, Card Eight H, Card Nine C, Card Ten S, Card Jack S]
+fourOfAKind :: Hand
+fourOfAKind  = [Card Two S, Card Two H, Card Two C, Card Two D, Card Jack S]
+
+fullHouse :: Hand
+fullHouse = [Card Two S, Card Two C, Card Seven S, Card Seven H, Card Seven D]
 
 flush :: Hand
 flush = [Card Two S, Card King S, Card Seven S, Card Ten S, Card Jack S]
 
+straight :: Hand
+straight = [Card Seven S, Card Eight H, Card Nine C, Card Ten S, Card Jack S]
+
+-- Tests
 tests :: TestTree
 tests = testGroup "Tests" [unitTests]
 
@@ -20,6 +28,10 @@ unitTests :: TestTree
 unitTests = testGroup "Unit tests"
     [ testCase "Can detect straight flush" $
         assertEqual "" 1 (getHandScore straightFlush),
+      testCase "Can detect four of a kind" $
+        assertEqual "" 2 (getHandScore fourOfAKind),
+      testCase "Can detect full house" $
+        assertEqual "" 3 (getHandScore fullHouse),
       testCase "Can detect flush not straight" $
         assertEqual "" 4 (getHandScore flush),
       testCase "Can detect straight not flush" $
