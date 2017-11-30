2605b11932558da25162fccf15b4d392d5df7078
Yi Zhang <yi.zhang7210@gmail.com>
2017-11-30 10:23:55 -0500

    Poker challenge: Determining winning hands in a table

--- PokerChallenge/src/Game.hs
+++ PokerChallenge/src/Game.hs
@@ -7,3 +7,11 @@ getBestHand :: [Card] -> Hand
 getBestHand theCards = maximum . map newHand $ zipWith (\\) (repeat theCards) twoCardCombos where
     twoCardCombos = [[c1, c2] | c1 <- theCards, c2 <- theCards, c1 /= c2]
 
+
+getWinningHand :: [Card] -> [[Card]] -> [Int]
+getWinningHand board pockets
+    | length board /= 5 = error "There has to be 5 cards on the board"
+    | length pockets > 10 || length pockets < 2 = error "Game has to be between 2-10 players"
+    | not $ all ((2==) . length) pockets  = error "Each player has to have 2 pocket cards"
+    | otherwise = maximum bestHands `elemIndices` bestHands where
+        bestHands = map getBestHand . map (++ board) $ pockets
--- PokerChallenge/test/TestGame.hs
+++ PokerChallenge/test/TestGame.hs
@@ -3,14 +3,23 @@ module TestGame where
 import Test.Tasty (defaultMain, testGroup, TestTree)
 import Test.Tasty.HUnit (assertEqual, assertBool, testCase)
 import Hands
-import Game (getBestHand)
+import Game (getBestHand, getWinningHand)
 import TestHands (fullHouse)
 
 sevenCards = [Card Two S, Card King S, Card Two C, Card Seven S, Card Queen H, Card Seven H, Card Seven D]
 
+boardCards = [Card Ace H, Card Eight S, Card Six S, Card Four S, Card Ace S]
+player1 = [Card King S, Card Queen S]
+player2 = [Card Eight C , Card Eight D]
+laChiffre = [Card Ace C , Card Six H]
+bond = [Card Seven S , Card Five S]
+
+
 gameTests :: TestTree
 gameTests = testGroup "Poker game tests"
     [ testCase "Can determine the best hand out of 7 cards" $
-        assertEqual "" fullHouse (getBestHand sevenCards)
+        assertEqual "" fullHouse (getBestHand sevenCards),
+      testCase "Can determine the best hand in Casino Royale" $
+        assertEqual "" [1, 3] (getWinningHand boardCards [player1, bond, player2, bond, laChiffre])
     ]
 
